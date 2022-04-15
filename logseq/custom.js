ebbd3c43ae808636bb5a3b519e1aea55678e8b1e
duanfei <duanfei.job@gmail.com>
2022-04-15 13:58:12 +0800

    changes on Fri Apr 15 13:58:12 CST 2022

---
+++ logseq/custom.js
@@ -0,0 +1,50 @@
+let currentRepo = logseq.api.get_current_graph().path;
+
+function isActive() {
+  return logseq.api.get_current_graph().path === currentRepo;
+}
+
+function isEditing() {
+  return logseq.api.check_editing();
+}
+
+function getActiveTextarea() {
+  if (isEditing()) {
+    const textarea = document.activeElement;
+    return textarea.nodeName === "TEXTAREA" ? textarea : undefined;
+  }
+}
+
+function quoteTextInTextarea(textarea, quote) {
+  if (isEditing()) {
+    const { selectionStart, selectionEnd } = textarea;
+    const selectedText = textarea.value.substring(selectionStart, selectionEnd);
+    if (selectedText) {
+      textarea.value =
+        textarea.value.substring(0, selectionStart) +
+        quote +
+        selectedText +
+        quote +
+        textarea.value.substring(selectionEnd);
+      textarea.selectionStart = selectionStart + 1;
+      textarea.selectionEnd = selectionEnd + 1;
+      return true;
+    }
+  }
+}
+
+window.addEventListener(
+  "keydown",
+  (e) => {
+    if (isActive() && e.key === '"') {
+      const textarea = getActiveTextarea();
+      if (textarea) {
+        const quoted = quoteTextInTextarea(textarea, '"');
+        if (quoted) {
+          e.stopPropagation();
+          e.preventDefault();
+        }
+      }
+    }
+  },
+);
