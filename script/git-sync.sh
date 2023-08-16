e81c34de6b1d1e0f7fe254f129c12fcfbdfa8d87
shaoanianche <github@duanfei.org>
2023-08-16 14:32:38 +0800

    changes on Wed Aug 16 14:32:37 UTC 2023

--- script/git-sync.sh
+++ script/git-sync.sh
@@ -6,13 +6,8 @@ PROJECT_ENV="$HOME/workspace/shaonianche/"
 
 cd "$PROJECT_ENV"
 
-CHANGED_FILES=$(git diff-index --cached HEAD --name-only --relative)
-if [ -n "$CHANGED_FILES" ]; then
-  git add -- "${CHANGED_FILES}"
-fi
-
 MESSAGE="changes on $(date)"
-
+git add .
 git commit -m "${MESSAGE}"
 
 git pull --rebase
